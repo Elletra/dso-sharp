da74fffabd33e70776fb9cce4e16763a786025b6
Electrk <Electrk@ymail.com>
2023-03-05 22:52:30 -0800

    Catch casting error and make some minor comment/function name changes

--- DSO Decompiler/src/Disassembler/Disassembler.cs
+++ DSO Decompiler/src/Disassembler/Disassembler.cs
@@ -1,4 +1,6 @@
-﻿using DSODecompiler.Loader;
+﻿using System;
+
+using DSODecompiler.Loader;
 using DSODecompiler.Opcodes;
 
 namespace DSODecompiler.Disassembler
@@ -23,15 +25,14 @@ namespace DSODecompiler.Disassembler
 			reader = new(data);
 			disassembly = new();
 			returnableValue = false;
-			reader.FunctionEnd = 0;
 
-			InitialDisassembly();
+			Disassemble();
 			MarkBranchTargets();
 
 			return disassembly;
 		}
 
-		protected void InitialDisassembly ()
+		protected void Disassemble ()
 		{
 			while (!reader.IsAtEnd)
 			{
@@ -63,10 +64,10 @@ namespace DSODecompiler.Disassembler
 		 * Right now we're just doing a linear sweep, but that doesn't handle certain anti-disassembly
 		 * techniques like jumping to the middle of an instruction.
 		 *
-		 * No DSO files do this currently to my knowledge, and probably never will, but I do want
+		 * No DSO files currently do this to my knowledge, and probably never will, but I do want
 		 * to support that eventually. I tried to with the initial version of this decompiler, but
 		 * it became too complicated too quickly, so I'm going to hold off on it for now. I just want
-		 * a working decompiler that works with what we have now (e.g. Blockland and The Forgettable Dungeon).
+		 * a working decompiler that works for what we have (e.g. Blockland and The Forgettable Dungeon).
 		 *
 		 * Maybe someday I will implement a recursive descent method instead, because right now it's
 		 * very easy to break this decompiler, but for now I'll just keep it simple...
@@ -76,19 +77,30 @@ namespace DSODecompiler.Disassembler
 		protected void DisassembleNext()
 		{
 			var addr = reader.Index;
-			var opcode = reader.Read();
-			var instruction = DisassembleOp((Opcode) opcode, addr);
+			var op = reader.Read();
+			var instruction = DisassembleOp(op, addr);
 
 			if (instruction == null)
 			{
-				throw new Exception($"Invalid opcode {opcode} at {addr}");
+				throw new Exception($"Invalid opcode {op} at {addr}");
 			}
 
 			ProcessInstruction(instruction);
 		}
 
-		protected Instruction DisassembleOp (Opcode opcode, uint addr)
+		protected Instruction DisassembleOp (uint op, uint addr)
 		{
+			Opcode opcode;
+
+			try
+			{
+				opcode = (Opcode) op;
+			}
+			catch (InvalidCastException)
+			{
+				return null;
+			}
+
 			ProcessAddress(addr);
 
 			return InstructionFactory.Create(opcode, addr, reader, returnableValue);
