d84c219a16699e18718ad0648df5a855c22b3beb
Electrk <Electrk@ymail.com>
2023-03-05 23:23:20 -0800

    Rename exception class, add `ProcessFunctionDecl` and `AddInstruction`

--- DSO Decompiler/src/Disassembler/Disassembler.cs
+++ DSO Decompiler/src/Disassembler/Disassembler.cs
@@ -7,11 +7,11 @@ namespace DSODecompiler.Disassembler
 {
 	public class Disassembler
 	{
-		public class Exception : System.Exception
+		public class DisassemblerException : Exception
 		{
-			public Exception () {}
-			public Exception (string message) : base(message) {}
-			public Exception (string message, System.Exception inner) : base(message, inner) {}
+			public DisassemblerException () {}
+			public DisassemblerException (string message) : base(message) {}
+			public DisassemblerException (string message, Exception inner) : base(message, inner) {}
 		}
 
 		protected Disassembly disassembly = null;
@@ -50,7 +50,7 @@ namespace DSODecompiler.Disassembler
 					/* See TODO below. */
 					if (!disassembly.Has(branch.TargetAddr))
 					{
-						throw new Exception($"Invalid branch target {branch.TargetAddr}");
+						throw new DisassemblerException($"Invalid branch target {branch.TargetAddr}");
 					}
 
 					disassembly[branch.TargetAddr].IsBranchTarget = true;
@@ -65,9 +65,9 @@ namespace DSODecompiler.Disassembler
 		 * techniques like jumping to the middle of an instruction.
 		 *
 		 * No DSO files currently do this to my knowledge, and probably never will, but I do want
-		 * to support that eventually. I tried to with the initial version of this decompiler, but
-		 * it became too complicated too quickly, so I'm going to hold off on it for now. I just want
-		 * a working decompiler that works for what we have (e.g. Blockland and The Forgettable Dungeon).
+		 * to support it eventually. I tried to with the initial version of this decompiler, but it
+		 * became too complicated too quickly, so I'm going to hold off on it for now. I just want a
+		 * working decompiler that works with what we have (e.g. Blockland and The Forgettable Dungeon).
 		 *
 		 * Maybe someday I will implement a recursive descent method instead, because right now it's
 		 * very easy to break this decompiler, but for now I'll just keep it simple...
@@ -82,7 +82,7 @@ namespace DSODecompiler.Disassembler
 
 			if (instruction == null)
 			{
-				throw new Exception($"Invalid opcode {op} at {addr}");
+				throw new DisassemblerException($"Invalid opcode {op} at {addr}");
 			}
 
 			ProcessInstruction(instruction);
@@ -117,28 +117,35 @@ namespace DSODecompiler.Disassembler
 		protected void ProcessInstruction (Instruction instruction)
 		{
 			ValidateInstruction(instruction);
-			SetReturnableValue(instruction);
 
-			disassembly.Add(instruction);
+			if (instruction is FuncDeclInsn func)
+			{
+				ProcessFunctionDecl(func);
+			}
+
+			SetReturnableValue(instruction);
+			AddInstruction(instruction);
 		}
 
 		protected void ValidateInstruction (Instruction instruction)
 		{
-			var opcode = instruction.Opcode;
 			var addr = instruction.Addr;
+			var opcode = instruction.Opcode;
+
+			// Just in case it wasn't caught during creation.
+			if (opcode.Type == OpcodeType.Invalid)
+			{
+				throw new DisassemblerException($"Opcode with invalid type at {addr}");
+			}
 
 			switch (instruction)
 			{
 				case FuncDeclInsn func:
 				{
-					if (func.HasBody)
+					/* See TODO in BytecodeReader class. */
+					if (func.HasBody && reader.InFunction)
 					{
-						if (reader.InFunction)
-						{
-							throw new Exception($"Nested function declaration at {addr}");
-						}
-
-						reader.FunctionEnd = func.EndAddr;
+						throw new DisassemblerException($"Nested function declaration at {addr}");
 					}
 
 					break;
@@ -148,12 +155,12 @@ namespace DSODecompiler.Disassembler
 				{
 					if (opcode.BranchType == BranchType.Invalid)
 					{
-						throw new Exception($"Invalid branch type at {addr}");
+						throw new DisassemblerException($"Invalid BranchType at {addr}");
 					}
 
 					if (branch.TargetAddr >= reader.CodeSize)
 					{
-						throw new Exception($"Branch at {addr} jumps to invalid address {branch.TargetAddr}");
+						throw new DisassemblerException($"Branch at {addr} jumps to invalid address {branch.TargetAddr}");
 					}
 
 					break;
@@ -165,7 +172,7 @@ namespace DSODecompiler.Disassembler
 
 					if (type == AdvanceStringType.Invalid)
 					{
-						throw new Exception($"Invalid advance string type at {addr}");
+						throw new DisassemblerException($"Invalid AdvanceStringType at {addr}");
 					}
 
 					break;
@@ -175,6 +182,14 @@ namespace DSODecompiler.Disassembler
 				{
 					break;
 				}
+			}
+		}
+
+		protected void ProcessFunctionDecl (FuncDeclInsn func)
+		{
+			if (func.HasBody)
+			{
+				reader.FunctionEnd = func.EndAddr;
 			}
 		}
 
@@ -200,6 +215,11 @@ namespace DSODecompiler.Disassembler
 					break;
 				}
 			}
+		}
+
+		protected void AddInstruction (Instruction instruction)
+		{
+			disassembly.Add(instruction);
 		}
 	}
 }
