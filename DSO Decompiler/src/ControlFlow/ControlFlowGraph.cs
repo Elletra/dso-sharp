ae3b6e70effeeb34fdea6fed8111ce7f9e0404c4
Electrk <Electrk@ymail.com>
2023-03-12 23:14:37 -0700

    Add `DirectedGraph` class and make `ControlFlowGraph` inherit from that

--- DSO Decompiler/src/ControlFlow/ControlFlowGraph.cs
+++ DSO Decompiler/src/ControlFlow/ControlFlowGraph.cs
@@ -2,15 +2,14 @@
 using System.Collections.Generic;
 
 using DSODecompiler.Disassembler;
+using DSODecompiler.Util;
 
 namespace DSODecompiler.ControlFlow
 {
-	public class ControlFlowNode
+	public class ControlFlowNode : DirectedGraph<uint, ControlFlowNode>.Node
 	{
 		public uint Addr { get; }
 		public List<Instruction> Instructions { get; } = new();
-		public List<ControlFlowNode> Predecessors { get; } = new();
-		public List<ControlFlowNode> Successors { get; } = new();
 
 		public Instruction FirstInstruction => Instructions[0];
 		public Instruction LastInstruction => Instructions[^1];
@@ -18,134 +17,10 @@ namespace DSODecompiler.ControlFlow
 		public ControlFlowNode (uint addr) => Addr = addr;
 	}
 
-	public class ControlFlowGraph
+	public class ControlFlowGraph : DirectedGraph<uint, ControlFlowNode>
 	{
-		private readonly Dictionary<uint, ControlFlowNode> nodes = new();
+		public override ControlFlowNode EntryPoint => Get(0);
 
-		public ControlFlowNode EntryPoint => Get(0);
-
-		public ControlFlowNode Add (uint addr)
-		{
-			if (Has(addr))
-			{
-				return Get(addr);
-			}
-
-			nodes[addr] = new(addr);
-
-			return nodes[addr];
-		}
-
-		public bool Has (uint addr) => nodes.ContainsKey(addr);
-		public ControlFlowNode Get (uint addr) => Has(addr) ? nodes[addr] : null;
-		public ControlFlowNode AddOrGet (uint addr) => Has(addr) ? Get(addr) : Add(addr);
-
-		/// <param name="from"></param>
-		/// <param name="to"></param>
-		/// <returns>false if either `from` or `to` nodes do not exist.</returns>
-		public bool AddEdge (uint from, uint to)
-		{
-			if (!Has(from) || !Has(to))
-			{
-				return false;
-			}
-
-			var nodeFrom = Get(from);
-			var nodeTo = Get(to);
-
-			nodeFrom.Successors.Add(nodeTo);
-			nodeTo.Predecessors.Add(nodeFrom);
-
-			return true;
-		}
-
-		/// <param name="from"></param>
-		/// <param name="to"></param>
-		/// <returns>false if either `from` or `to` nodes do not exist.</returns>
-		public bool RemoveEdge (uint from, uint to)
-		{
-			if (!Has(from) || !Has(to))
-			{
-				return false;
-			}
-
-			var nodeFrom = Get(from);
-			var nodeTo = Get(to);
-
-			nodeFrom.Successors.Remove(nodeTo);
-			nodeTo.Predecessors.Remove(nodeFrom);
-
-			return true;
-		}
-
-		public bool HasEdge (uint from, uint to)
-		{
-			if (!Has(from) || !Has(to))
-			{
-				return false;
-			}
-
-			var nodeFrom = Get(from);
-			var nodeTo = Get(to);
-
-			return nodeFrom.Successors.Contains(nodeTo) && nodeTo.Predecessors.Contains(nodeFrom);
-		}
-
-		public void AddEdge (ControlFlowNode from, ControlFlowNode to)
-		{
-			from.Successors.Add(to);
-			to.Predecessors.Add(from);
-		}
-
-		public void RemoveEdge (ControlFlowNode from, ControlFlowNode to)
-		{
-			from.Successors.Remove(to);
-			to.Predecessors.Remove(from);
-		}
-
-		public bool HasEdge (ControlFlowNode from, ControlFlowNode to) => from.Successors.Contains(to);
-
-		/// <summary>
-		/// Iterates over all the nodes, even if they're not connected to anything.
-		/// </summary>
-		/// <returns></returns>
-		public IEnumerator<ControlFlowNode> GetEnumerator ()
-		{
-			foreach (var pair in nodes)
-			{
-				yield return pair.Value;
-			}
-		}
-
-		public IEnumerable<ControlFlowNode> PreorderDFS ()
-		{
-			var node = EntryPoint;
-			var visited = new HashSet<uint>();
-			var queue = new Queue<ControlFlowNode>();
-
-			queue.Enqueue(node);
-
-			while (queue.Count > 0)
-			{
-				node = queue.Dequeue();
-
-				yield return node;
-
-				visited.Add(node.Addr);
-
-				foreach (var successor in node.Successors)
-				{
-					if (!visited.Contains(successor.Addr))
-					{
-						queue.Enqueue(successor);
-					}
-				}
-			}
-		}
-
-		public IEnumerable<ControlFlowNode> PostorderDFS ()
-		{
-			throw new NotImplementedException($"TODO: Implement me!");
-		}
+		public ControlFlowNode AddOrGet (uint addr) => Has(addr) ? Get(addr) : Add(addr, new ControlFlowNode(addr));
 	}
 }
---
+++ DSO Decompiler/src/Util/DirectedGraph.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DSODecompiler.Util
+{
+	public class DirectedGraph<K, N> where N : DirectedGraph<K, N>.Node
+	{
+		public class Node
+		{
+			public List<N> Predecessors { get; } = new();
+			public List<N> Successors { get; } = new();
+		}
+
+		private readonly Dictionary<K, N> nodes = new();
+
+		public virtual N EntryPoint
+		{
+			get
+			{
+				foreach (var pair in nodes)
+				{
+					return pair.Value;
+				}
+
+				return null;
+			}
+		}
+
+		public N Add (K key, N node)
+		{
+			if (Has(key))
+			{
+				return Get(key);
+			}
+
+			nodes[key] = node;
+
+			return node;
+		}
+
+		public bool Has (K key) => nodes.ContainsKey(key);
+		public N Get (K key) => Has(key) ? nodes[key] : null;
+
+		/// <param name="from"></param>
+		/// <param name="to"></param>
+		/// <returns>false if either `from` or `to` nodes do not exist.</returns>
+		public bool AddEdge (K from, K to)
+		{
+			if (!Has(from) || !Has(to))
+			{
+				return false;
+			}
+
+			var nodeFrom = Get(from);
+			var nodeTo = Get(to);
+
+			nodeFrom.Successors.Add(nodeTo);
+			nodeTo.Predecessors.Add(nodeFrom);
+
+			return true;
+		}
+
+		/// <param name="from"></param>
+		/// <param name="to"></param>
+		/// <returns>false if either `from` or `to` nodes do not exist.</returns>
+		public bool RemoveEdge (K from, K to)
+		{
+			if (!Has(from) || !Has(to))
+			{
+				return false;
+			}
+
+			var nodeFrom = Get(from);
+			var nodeTo = Get(to);
+
+			nodeFrom.Successors.Remove(nodeTo);
+			nodeTo.Predecessors.Remove(nodeFrom);
+
+			return true;
+		}
+
+		public bool HasEdge (K from, K to)
+		{
+			if (!Has(from) || !Has(to))
+			{
+				return false;
+			}
+
+			var nodeFrom = Get(from);
+			var nodeTo = Get(to);
+
+			return nodeFrom.Successors.Contains(nodeTo) && nodeTo.Predecessors.Contains(nodeFrom);
+		}
+
+		public void AddEdge (N from, N to)
+		{
+			from.Successors.Add(to);
+			to.Predecessors.Add(from);
+		}
+
+		public void RemoveEdge (N from, N to)
+		{
+			from.Successors.Remove(to);
+			to.Predecessors.Remove(from);
+		}
+
+		public bool HasEdge (N from, N to) => from.Successors.Contains(to);
+
+		/// <summary>
+		/// Iterates over all the nodes, even if they're not connected to anything.
+		/// </summary>
+		/// <returns></returns>
+		public IEnumerator<N> GetEnumerator ()
+		{
+			foreach (var pair in nodes)
+			{
+				yield return pair.Value;
+			}
+		}
+
+		public IEnumerable<N> PreorderDFS ()
+		{
+			var node = EntryPoint;
+			var visited = new HashSet<N>();
+			var queue = new Queue<N>();
+
+			queue.Enqueue(node);
+
+			while (queue.Count > 0)
+			{
+				node = queue.Dequeue();
+
+				yield return node;
+
+				visited.Add(node);
+
+				foreach (var successor in node.Successors)
+				{
+					if (!visited.Contains(successor))
+					{
+						queue.Enqueue(successor);
+					}
+				}
+			}
+		}
+
+		public IEnumerable<N> PostorderDFS ()
+		{
+			throw new NotImplementedException($"TODO: Implement me!");
+		}
+	}
+}
