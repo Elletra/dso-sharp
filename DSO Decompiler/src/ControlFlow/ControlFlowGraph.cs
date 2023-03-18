2b4764bced6c23c2c8f5a9465756bb9fe91186c9
Electrk <Electrk@ymail.com>
2023-03-18 01:26:17 -0700

    Remove `IsLoopStart` and `IsLoopEnd`, and remove `GetLoopCount()`

--- DSO Decompiler/src/ControlFlow/ControlFlowGraph.cs
+++ DSO Decompiler/src/ControlFlow/ControlFlowGraph.cs
@@ -6,13 +6,10 @@ using DSODecompiler.Util;
 
 namespace DSODecompiler.ControlFlow
 {
-	public class ControlFlowNode : DirectedGraph<uint, ControlFlowNode>.Node
+	public class ControlFlowNode : GraphNode
 	{
 		public uint Addr { get; }
 		public List<Instruction> Instructions { get; } = new();
-
-		public bool IsLoopStart { get; set; } = false;
-		public bool IsLoopEnd { get; set; } = false;
 
 		public Instruction FirstInstruction => Instructions.Count > 0 ? Instructions[0] : null;
 		public Instruction LastInstruction => Instructions.Count > 0 ? Instructions[^1] : null;
@@ -28,22 +25,5 @@ namespace DSODecompiler.ControlFlow
 
 		public ControlFlowGraph (FunctionInstruction func = null) => FunctionHeader = func;
 		public ControlFlowNode AddOrGet (uint addr) => Has(addr) ? Get(addr) : Add(addr, new ControlFlowNode(addr));
-
-		// TODO: A better way would perhaps be to cache the value instead of looping every time...
-		public uint GetLoopCount ()
-		{
-			uint count = 0;
-
-			foreach (var node in nodes.Values)
-			{
-				// We use `IsLoopEnd` because multiple loops can share the same start block.
-				if (node.IsLoopEnd)
-				{
-					count++;
-				}
-			}
-
-			return count;
-		}
 	}
 }
