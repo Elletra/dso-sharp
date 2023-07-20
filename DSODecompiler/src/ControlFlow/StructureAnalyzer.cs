a22f601ded524212a63c5b5f783e2f07127860b9
Electrk <Electrk@ymail.com>
2023-07-19 23:45:44 -0700

    Autoformat

--- DSODecompiler/src/ControlFlow/StructureAnalyzer.cs
+++ DSODecompiler/src/ControlFlow/StructureAnalyzer.cs
@@ -34,9 +34,9 @@ namespace DSODecompiler.ControlFlow
 	{
 		public class Exception : System.Exception
 		{
-			public Exception () {}
-			public Exception (string message) : base(message) {}
-			public Exception (string message, Exception inner) : base(message, inner) {}
+			public Exception () { }
+			public Exception (string message) : base(message) { }
+			public Exception (string message, Exception inner) : base(message, inner) { }
 		}
 
 		protected ControlFlowGraph graph;
