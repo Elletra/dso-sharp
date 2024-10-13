66915090b59127c9910bbeb6a5a8220667461d1d
Elletra <elletra@proton.me>
2024-10-13 15:00:53 -0700

    Implement assignment and binary expression code generation

--- AST/Nodes/AssignmentNode.cs
+++ AST/Nodes/AssignmentNode.cs
@@ -1,16 +1,71 @@
-﻿using DSO.Opcodes;
+﻿using DSO.CodeGenerator;
+using DSO.Opcodes;
 
 namespace DSO.AST.Nodes
 {
 	public class AssignmentNode(Node left, Node right, Opcode? op = null) : Node(NodeType.ExpressionStatement)
-    {
-        public readonly Node Left = left;
-        public readonly Node Right = right;
-        public readonly Opcode? Operator = op;
+	{
+		public readonly Node Left = left;
+		public readonly Node Right = right;
+		public readonly Opcode? Operator = op;
 
-        public override bool Equals(object? obj) => base.Equals(obj) && obj is AssignmentNode node
-            && node.Left.Equals(Left) && node.Right.Equals(Right) && Equals(node.Operator, Operator);
+		public override bool Equals(object? obj) => base.Equals(obj) && obj is AssignmentNode node
+			&& node.Left.Equals(Left) && node.Right.Equals(Right) && Equals(node.Operator, Operator);
 
-        public override int GetHashCode() => base.GetHashCode() ^ Left.GetHashCode() ^ Right.GetHashCode() ^ (Operator?.GetHashCode() ?? 0);
-    }
+		public override int GetHashCode() => base.GetHashCode() ^ Left.GetHashCode() ^ Right.GetHashCode() ^ (Operator?.GetHashCode() ?? 0);
+
+		public override void Visit(TokenStream stream, bool isExpression)
+		{
+			Left.Visit(stream, isExpression: true);
+
+			if (Operator == null)
+			{
+				stream.Write("=");
+				Right.Visit(stream, isExpression: true);
+			}
+			else
+			{
+				var op = Operator.Value;
+				var incDec = false;
+
+				if (Right is ConstantNode<double> constant && constant.Value == 1.0f)
+				{
+					if (op == Ops.OP_ADD)
+					{
+						incDec = true;
+						stream.Write("++");
+					}
+					else if (op == Ops.OP_SUB)
+					{
+						incDec = true;
+						stream.Write("--");
+					}
+				}
+
+				if (!incDec)
+				{
+					stream.Write($"{op switch
+					{
+						Ops.OP_ADD => "+",
+						Ops.OP_SUB => "-",
+						Ops.OP_MUL => "*",
+						Ops.OP_DIV => "/",
+						Ops.OP_MOD => "%",
+						Ops.OP_BITOR => "|",
+						Ops.OP_BITAND => "&",
+						Ops.OP_XOR => "^",
+						Ops.OP_SHL => "<<",
+						Ops.OP_SHR => ">>",
+					}}=");
+
+					Right.Visit(stream, isExpression: true);
+				}
+			}
+
+			if (!isExpression)
+			{
+				stream.Write(";", "\n");
+			}
+		}
+	}
 }
--- AST/Nodes/BinaryNode.cs
+++ AST/Nodes/BinaryNode.cs
@@ -1,18 +1,48 @@
-﻿using DSO.Opcodes;
+﻿using DSO.CodeGenerator;
+using DSO.Opcodes;
 
 namespace DSO.AST.Nodes
 {
 	public class BinaryNode(Node left, Node right, Opcode op) : Node(NodeType.Expression)
-    {
-        public readonly Node Left = left;
-        public readonly Node Right = right;
-        public readonly Opcode Op = op;
+	{
+		public readonly Node Left = left;
+		public readonly Node Right = right;
+		public readonly Opcode Op = op;
 
-        public override bool Equals(object? obj) => base.Equals(obj) && obj is BinaryNode node
-            && node.Left.Equals(Left) && node.Right.Equals(Right) && node.Op.Equals(Op);
+		public override bool Equals(object? obj) => base.Equals(obj) && obj is BinaryNode node
+			&& node.Left.Equals(Left) && node.Right.Equals(Right) && node.Op.Equals(Op);
 
-        public override int GetHashCode() => base.GetHashCode() ^ Left.GetHashCode() ^ Right.GetHashCode() ^ Op.GetHashCode();
-    }
+		public override int GetHashCode() => base.GetHashCode() ^ Left.GetHashCode() ^ Right.GetHashCode() ^ Op.GetHashCode();
+
+		public override void Visit(TokenStream stream, bool isExpression)
+		{
+			Left.Visit(stream, isExpression: true);
+
+			stream.Write(Op.Value switch
+			{
+				Ops.OP_ADD => "+",
+				Ops.OP_SUB => "-",
+				Ops.OP_MUL => "*",
+				Ops.OP_DIV => "/",
+				Ops.OP_CMPLT => "<",
+				Ops.OP_CMPGR => ">",
+				Ops.OP_MOD => "%",
+				Ops.OP_BITOR => "|",
+				Ops.OP_BITAND => "&",
+				Ops.OP_XOR => "^",
+				Ops.OP_CMPEQ => "==",
+				Ops.OP_CMPNE => "!=",
+				Ops.OP_CMPLE => "<=",
+				Ops.OP_CMPGE => ">=",
+				Ops.OP_SHL => "<<",
+				Ops.OP_SHR => ">>",
+				Ops.OP_JMPIF_NP => "||",
+				Ops.OP_JMPIFNOT_NP => "&&",
+			});
+
+			Right.Visit(stream, isExpression: true);
+		}
+	}
 
 	public class BinaryStringNode(Node left, Node right, Opcode op, bool not = false) : BinaryNode(left, right, op)
 	{
@@ -20,5 +50,12 @@ namespace DSO.AST.Nodes
 
 		public override bool Equals(object? obj) => base.Equals(obj) && obj is BinaryStringNode node && node.Not.Equals(Not);
 		public override int GetHashCode() => base.GetHashCode() ^ Not.GetHashCode();
+
+		public override void Visit(TokenStream stream, bool isExpression)
+		{
+			Left.Visit(stream, isExpression: true);
+			stream.Write(Not ? "!$=" : "$=");
+			Right.Visit(stream, isExpression: true);
+		}
 	}
 }
