f0925e6a51a9cb014f0ef2075da75db2cf562d1b
Elletra <elletra@proton.me>
2024-10-13 15:44:19 -0700

    Add more `Write()` overloads

--- CodeGenerator/TokenStream.cs
+++ CodeGenerator/TokenStream.cs
@@ -1,9 +1,29 @@
-﻿namespace DSO.CodeGenerator
+﻿using DSO.AST.Nodes;
+
+namespace DSO.CodeGenerator
 {
 	public class TokenStream
 	{
 		public readonly List<string> Stream = [];
 
 		public void Write(params string[] tokens) => Stream.AddRange(tokens);
+		public void Write(Node node, bool isExpression) => node.Visit(this, isExpression);
+
+		public void Write(Node node, Node parent)
+		{
+			var addParentheses = node.ShouldAddParentheses(parent);
+
+			if (addParentheses)
+			{
+				Write("(");
+			}
+
+			node.Visit(this, isExpression: true);
+
+			if (addParentheses)
+			{
+				Write(")");
+			}
+		}
 	}
 }
