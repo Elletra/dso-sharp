6c53fcff29c804cd8eb688d6182a99e4f9813e2b
Elletra <elletra@proton.me>
2024-10-13 15:01:06 -0700

    Implement variable code generation

--- AST/Nodes/VariableNode.cs
+++ AST/Nodes/VariableNode.cs
@@ -1,11 +1,25 @@
-﻿namespace DSO.AST.Nodes
+﻿using DSO.CodeGenerator;
+
+namespace DSO.AST.Nodes
 {
 	public class VariableNode(string name, Node? index = null) : Node(NodeType.Expression)
-    {
-        public readonly string Name = name;
-        public Node? Index { get; set; } = index;
+	{
+		public readonly string Name = name;
+		public Node? Index { get; set; } = index;
 
-        public override bool Equals(object? obj) => base.Equals(obj) && obj is VariableNode node && node.Name.Equals(Name) && Equals(node.Index, Index);
-        public override int GetHashCode() => base.GetHashCode() ^ Name.GetHashCode() ^ (Index?.GetHashCode() ?? 0);
-    }
+		public override bool Equals(object? obj) => base.Equals(obj) && obj is VariableNode node && node.Name.Equals(Name) && Equals(node.Index, Index);
+		public override int GetHashCode() => base.GetHashCode() ^ Name.GetHashCode() ^ (Index?.GetHashCode() ?? 0);
+
+		public override void Visit(TokenStream stream, bool isExpression)
+		{
+			stream.Write(Name);
+
+			if (Index != null)
+			{
+				stream.Write("[");
+				Index.Visit(stream, isExpression: true);
+				stream.Write("]");
+			}
+		}
+	}
 }
