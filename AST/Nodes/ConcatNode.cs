d1b90d7730bc8c8e804cdd82953d6cf8945c5a78
Elletra <elletra@proton.me>
2024-10-13 15:44:53 -0700

    Implement concatenation code generation

--- AST/Nodes/ConcatNode.cs
+++ AST/Nodes/ConcatNode.cs
@@ -1,24 +1,43 @@
-﻿namespace DSO.AST.Nodes
+﻿using DSO.CodeGenerator;
+
+namespace DSO.AST.Nodes
 {
 	public class ConcatNode(Node left, char? ch = null) : Node(NodeType.Expression)
-    {
-        public readonly Node Left = left;
+	{
+		public readonly Node Left = left;
 
-        private Node _right = null;
+		private Node _right = null;
 
-        public Node Right
-        {
-            get => _right;
-            set => _right ??= value;
-        }
+		public Node Right
+		{
+			get => _right;
+			set => _right ??= value;
+		}
 
-        public readonly char? Char = ch;
+		public readonly char? Char = ch;
 
-        public override bool Equals(object? obj) => base.Equals(obj) && obj is ConcatNode node
-            && node.Left.Equals(Left) && Equals(node._right, _right) && Equals(node.Char, Char);
+		public override bool Equals(object? obj) => base.Equals(obj) && obj is ConcatNode node
+			&& node.Left.Equals(Left) && Equals(node._right, _right) && Equals(node.Char, Char);
 
-        public override int GetHashCode() => base.GetHashCode() ^ Left.GetHashCode() ^ (_right?.GetHashCode() ?? 0) ^ (Char?.GetHashCode() ?? 0);
-    }
+		public override int GetHashCode() => base.GetHashCode() ^ Left.GetHashCode() ^ (_right?.GetHashCode() ?? 0) ^ (Char?.GetHashCode() ?? 0);
+
+		public override void Visit(TokenStream stream, bool isExpression)
+		{
+			stream.Write(Left, this);
+
+			if (Char != null)
+			{
+				stream.Write(Char switch
+				{
+					' ' => "SPC",
+					'\t' => "TAB",
+					'\n' => "NL",
+				});
+			}
+
+			stream.Write(Right, this);
+		}
+	}
 
 	public class CommaConcatNode : ConcatNode
 	{
@@ -29,5 +48,12 @@
 
 		public override bool Equals(object? obj) => base.Equals(obj) && obj is CommaConcatNode;
 		public override int GetHashCode() => base.GetHashCode();
+
+		public override void Visit(TokenStream stream, bool isExpression)
+		{
+			stream.Write(Left, this);
+			stream.Write(",");
+			stream.Write(Right, this);
+		}
 	}
 }
