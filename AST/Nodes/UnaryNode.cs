455f0d300afb0f30e88e1f6831b2257cbc8c1f6c
Elletra <elletra@proton.me>
2024-10-13 17:47:16 -0700

    Implement unary expression code generation

--- AST/Nodes/UnaryNode.cs
+++ AST/Nodes/UnaryNode.cs
@@ -1,13 +1,26 @@
-﻿using DSO.Opcodes;
+﻿using DSO.CodeGenerator;
+using DSO.Opcodes;
 
 namespace DSO.AST.Nodes
 {
 	public class UnaryNode(Node node, Opcode op) : Node(NodeType.Expression)
-    {
-        public readonly Node Node = node;
-        public readonly Opcode Op = op;
+	{
+		public readonly Node Node = node;
+		public readonly Opcode Op = op;
 
-        public override bool Equals(object? obj) => base.Equals(obj) && obj is UnaryNode node && node.Node.Equals(Node) && node.Op.Equals(Op);
-        public override int GetHashCode() => base.GetHashCode() ^ Node.GetHashCode() ^ Op.GetHashCode();
-    }
+		public override bool Equals(object? obj) => base.Equals(obj) && obj is UnaryNode node && node.Node.Equals(Node) && node.Op.Equals(Op);
+		public override int GetHashCode() => base.GetHashCode() ^ Node.GetHashCode() ^ Op.GetHashCode();
+
+		public override void Visit(TokenStream stream, bool isExpression)
+		{
+			stream.Write(Op.Value switch
+			{
+				Ops.OP_NEG => "-",
+				Ops.OP_ONESCOMPLEMENT => "~",
+				Ops.OP_NOT or Ops.OP_NOTF => "!",
+			});
+
+			stream.Write(Node, this);
+		}
+	}
 }
