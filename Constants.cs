b9e1d50ef3c443722a63e7a1d8ac73846c8076ef
Elletra <elletra@proton.me>
2024-10-16 22:04:19 -0700

    Version 1.0.0

--- Constants.cs
+++ Constants.cs
@@ -13,7 +13,7 @@ namespace DSO.Constants
     static public class Decompiler
     {
         public const string AUTHOR = "Elletra";
-        public const string VERSION = "0.1.0";
+        public const string VERSION = "1.0.0";
         public const string EXTENSION = ".dso";
 
         static public class GameVersions
